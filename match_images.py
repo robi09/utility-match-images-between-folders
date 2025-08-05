3863b9d554c79e64c401c8844b7b1320e08da1e1
Robert Hapiuc @ MacM1 <roberthapiuc@gmail.com>
2025-08-05 22:54:45 +0300

    arguments + readme

--- match_images.py
+++ match_images.py
@@ -1,13 +1,28 @@
 import os
 import shutil
+import sys
 
-left_root = '../images_original'
-right_flat = '../images_new'
+if len(sys.argv) != 3:
+    print("Usage: python update_images.py <left_root_folder> <right_flat_folder>")
+    sys.exit(1)
+
+left_root = sys.argv[1]
+right_flat = sys.argv[2]
+
+# Validate paths
+if not os.path.isdir(left_root):
+    print(f"Error: '{left_root}' is not a valid folder.")
+    sys.exit(1)
+
+if not os.path.isdir(right_flat):
+    print(f"Error: '{right_flat}' is not a valid folder.")
+    sys.exit(1)
 
 # Get list of all image files in the right folder
 right_images = {filename for filename in os.listdir(right_flat) if os.path.isfile(os.path.join(right_flat, filename))}
 
 # Walk through the left_root folder structure
+replaced_count = 0
 for dirpath, _, filenames in os.walk(left_root):
     for filename in filenames:
         if filename in right_images:
@@ -17,3 +32,6 @@ for dirpath, _, filenames in os.walk(left_root):
             # Replace the file
             shutil.copy2(right_file_path, left_file_path)
             print(f"Replaced: {left_file_path}")
+            replaced_count += 1
+
+print(f"✅ Done. {replaced_count} files replaced.")
